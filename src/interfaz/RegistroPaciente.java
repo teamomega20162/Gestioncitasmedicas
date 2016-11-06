60c97032ff44d398df7f241eaceb519529102ddc
AlexDeveloper714 <idiazcruz@uniminuto.edu.co>
2016-11-06 13:02:56 -0500

    Revision Interfaces

--- src/interfaz/RegistroPaciente.java
+++ src/interfaz/RegistroPaciente.java
@@ -1,11 +1,35 @@
 package interfaz;
 
+import Atxy2k.CustomTextField.RestrictedTextField;
+import java.util.Arrays;
+import java.util.Date;
+
 public class RegistroPaciente extends javax.swing.JFrame {
 
     public RegistroPaciente() {
         initComponents();
         this.setLocationRelativeTo(null);
+        RestrictedTextField numero = new RestrictedTextField(telefonoTxt, "0123456789");
+        RestrictedTextField numero2 = new RestrictedTextField(celularTxt, "0123456789");
+        RestrictedTextField numero3 = new RestrictedTextField(documentoTxt, "0123456789");
+        RestrictedTextField texto1 = new RestrictedTextField(nombreTxt);
+        texto1.setOnlyText(true);
+        RestrictedTextField texto2 = new RestrictedTextField(apellidoTxt);
+        texto2.setOnlyText(true);
     }
+
+    private String nombre;
+    private String documento;
+    private String tipoDocumento;
+    private String apellido;
+    private Date fechaNacimiento;
+    private String direccion;
+    private String eps;
+    private String telefono;
+    private String celular;
+    private String correo;
+    private String clave;
+    private String confirmar;
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -22,22 +46,23 @@ public class RegistroPaciente extends javax.swing.JFrame {
         correo_Lb = new javax.swing.JLabel();
         clave_Lb = new javax.swing.JLabel();
         confirmarClave_Lb = new javax.swing.JLabel();
-        documento_TextF = new javax.swing.JTextField();
-        tipoDocumento_ComboB = new javax.swing.JComboBox<>();
-        nombres_TextF = new javax.swing.JTextField();
-        apellidos_TextF = new javax.swing.JTextField();
-        direcccion_TextF = new javax.swing.JTextField();
-        telefono_TextF = new javax.swing.JTextField();
-        celular_TextF = new javax.swing.JTextField();
-        correo_TextF = new javax.swing.JTextField();
-        contraseña_PassF = new javax.swing.JPasswordField();
-        confContraseña_PassF = new javax.swing.JPasswordField();
+        documentoTxt = new javax.swing.JTextField();
+        tipoDocumentoBox = new javax.swing.JComboBox<>();
+        nombreTxt = new javax.swing.JTextField();
+        apellidoTxt = new javax.swing.JTextField();
+        direccionTxt = new javax.swing.JTextField();
+        telefonoTxt = new javax.swing.JTextField();
+        celularTxt = new javax.swing.JTextField();
+        correoTxt = new javax.swing.JTextField();
+        claveTxt = new javax.swing.JPasswordField();
+        confirmarTxt = new javax.swing.JPasswordField();
         registrarPaciente_Btn = new javax.swing.JButton();
         regresar_Btn = new javax.swing.JButton();
         titulo_Lb = new javax.swing.JLabel();
         fechaNacimiento_DateC = new com.toedter.calendar.JDateChooser();
         eps_Lb = new javax.swing.JLabel();
         eps_ComboB = new javax.swing.JComboBox<>();
+        mostrarPassBtn = new javax.swing.JToggleButton();
         fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
@@ -100,41 +125,46 @@ public class RegistroPaciente extends javax.swing.JFrame {
         confirmarClave_Lb.setText("Confirmar clave:");
         getContentPane().add(confirmarClave_Lb, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 310, -1, -1));
 
-        documento_TextF.setBackground(new java.awt.Color(204, 204, 255));
-        getContentPane().add(documento_TextF, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 100, 110, 30));
+        documentoTxt.setBackground(new java.awt.Color(204, 204, 255));
+        getContentPane().add(documentoTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 100, 110, 30));
 
-        tipoDocumento_ComboB.setBackground(new java.awt.Color(204, 204, 255));
-        tipoDocumento_ComboB.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "C.C.", "T.I." }));
-        getContentPane().add(tipoDocumento_ComboB, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 140, 110, 30));
+        tipoDocumentoBox.setBackground(new java.awt.Color(204, 204, 255));
+        tipoDocumentoBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "C.C.", "T.I." }));
+        getContentPane().add(tipoDocumentoBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 140, 110, 30));
 
-        nombres_TextF.setBackground(new java.awt.Color(204, 204, 255));
-        getContentPane().add(nombres_TextF, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 180, 110, 30));
+        nombreTxt.setBackground(new java.awt.Color(204, 204, 255));
+        getContentPane().add(nombreTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 180, 110, 30));
 
-        apellidos_TextF.setBackground(new java.awt.Color(204, 204, 255));
-        getContentPane().add(apellidos_TextF, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 220, 110, 30));
+        apellidoTxt.setBackground(new java.awt.Color(204, 204, 255));
+        getContentPane().add(apellidoTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 220, 110, 30));
 
-        direcccion_TextF.setBackground(new java.awt.Color(204, 204, 255));
-        getContentPane().add(direcccion_TextF, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 300, 110, 30));
+        direccionTxt.setBackground(new java.awt.Color(204, 204, 255));
+        getContentPane().add(direccionTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 300, 110, 30));
 
-        telefono_TextF.setBackground(new java.awt.Color(204, 204, 255));
-        getContentPane().add(telefono_TextF, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 150, 110, 30));
+        telefonoTxt.setBackground(new java.awt.Color(204, 204, 255));
+        getContentPane().add(telefonoTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 150, 110, 30));
 
-        celular_TextF.setBackground(new java.awt.Color(204, 204, 255));
-        getContentPane().add(celular_TextF, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 190, 110, 30));
+        celularTxt.setBackground(new java.awt.Color(204, 204, 255));
+        getContentPane().add(celularTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 190, 110, 30));
 
-        correo_TextF.setBackground(new java.awt.Color(204, 204, 255));
-        getContentPane().add(correo_TextF, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 230, 110, 30));
+        correoTxt.setBackground(new java.awt.Color(204, 204, 255));
+        getContentPane().add(correoTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 230, 110, 30));
 
-        contraseña_PassF.setBackground(new java.awt.Color(204, 204, 255));
-        getContentPane().add(contraseña_PassF, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 270, 110, 30));
+        claveTxt.setBackground(new java.awt.Color(204, 204, 255));
+        getContentPane().add(claveTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 270, 110, 30));
 
-        confContraseña_PassF.setBackground(new java.awt.Color(204, 204, 255));
-        getContentPane().add(confContraseña_PassF, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 310, 110, 30));
+        confirmarTxt.setBackground(new java.awt.Color(204, 204, 255));
+        getContentPane().add(confirmarTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 310, 110, 30));
 
         registrarPaciente_Btn.setBackground(new java.awt.Color(204, 204, 255));
         registrarPaciente_Btn.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         registrarPaciente_Btn.setForeground(new java.awt.Color(102, 102, 102));
         registrarPaciente_Btn.setText("Registrar Paciente");
+        registrarPaciente_Btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                registrarPaciente_BtnActionPerformed(evt);
+            }
+        });
         getContentPane().add(registrarPaciente_Btn, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 360, -1, 30));
 
         regresar_Btn.setBackground(new java.awt.Color(204, 204, 255));
@@ -164,6 +194,15 @@ public class RegistroPaciente extends javax.swing.JFrame {
         eps_ComboB.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Famisanar", "Capital Salud", "Cafe Salud", "SaludCoop" }));
         getContentPane().add(eps_ComboB, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 100, 110, 30));
 
+        mostrarPassBtn.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        mostrarPassBtn.setText("Mostrar Contraseña");
+        mostrarPassBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mostrarPassBtnActionPerformed(evt);
+            }
+        });
+        getContentPane().add(mostrarPassBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 360, -1, -1));
+
         fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Fondo2.jpg"))); // NOI18N
         getContentPane().add(fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 520, 410));
 
@@ -176,36 +215,60 @@ public class RegistroPaciente extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_regresar_BtnActionPerformed
 
-    
+    private void registrarPaciente_BtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_registrarPaciente_BtnActionPerformed
+        nombre = nombreTxt.getText();
+        apellido = apellidoTxt.getText();
+        documento = documentoTxt.getText();
+        tipoDocumento = tipoDocumentoBox.getSelectedItem().toString();
+        direccion = direccionTxt.getText();
+        eps = eps_ComboB.getSelectedItem().toString();
+        telefono = telefonoTxt.getText();
+        celular = celularTxt.getText();
+        correo = correoTxt.getText();
+        clave = Arrays.toString(claveTxt.getPassword());
+        confirmar = Arrays.toString(confirmarTxt.getPassword());
+    }//GEN-LAST:event_registrarPaciente_BtnActionPerformed
+
+    private void mostrarPassBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mostrarPassBtnActionPerformed
+        if (mostrarPassBtn.isSelected()) {
+            claveTxt.setEchoChar((char) 0);
+            confirmarTxt.setEchoChar((char) 0);
+        } else {
+            claveTxt.setEchoChar('*');
+            confirmarTxt.setEchoChar('*');
+        }
+    }//GEN-LAST:event_mostrarPassBtnActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField apellidoTxt;
     private javax.swing.JLabel apellidos_Lb;
-    private javax.swing.JTextField apellidos_TextF;
+    private javax.swing.JTextField celularTxt;
     private javax.swing.JLabel celular_Lb;
-    private javax.swing.JTextField celular_TextF;
+    private javax.swing.JPasswordField claveTxt;
     private javax.swing.JLabel clave_Lb;
-    private javax.swing.JPasswordField confContraseña_PassF;
     private javax.swing.JLabel confirmarClave_Lb;
-    private javax.swing.JPasswordField contraseña_PassF;
+    private javax.swing.JPasswordField confirmarTxt;
+    private javax.swing.JTextField correoTxt;
     private javax.swing.JLabel correo_Lb;
-    private javax.swing.JTextField correo_TextF;
-    private javax.swing.JTextField direcccion_TextF;
+    private javax.swing.JTextField direccionTxt;
     private javax.swing.JLabel direccion_Lb;
+    private javax.swing.JTextField documentoTxt;
     private javax.swing.JLabel documento_Lb;
-    private javax.swing.JTextField documento_TextF;
     private javax.swing.JComboBox<String> eps_ComboB;
     private javax.swing.JLabel eps_Lb;
     private javax.swing.JLabel fechaDeNacimiento_Lb;
     private com.toedter.calendar.JDateChooser fechaNacimiento_DateC;
     private javax.swing.JLabel fondo;
+    private javax.swing.JToggleButton mostrarPassBtn;
+    private javax.swing.JTextField nombreTxt;
     private javax.swing.JLabel nombres_Lb;
-    private javax.swing.JTextField nombres_TextF;
     private javax.swing.JButton registrarPaciente_Btn;
     private javax.swing.JButton regresar_Btn;
+    private javax.swing.JTextField telefonoTxt;
     private javax.swing.JLabel telefono_Lb;
-    private javax.swing.JTextField telefono_TextF;
     private javax.swing.JLabel tipoDeDocumento_Lb;
-    private javax.swing.JComboBox<String> tipoDocumento_ComboB;
+    private javax.swing.JComboBox<String> tipoDocumentoBox;
     private javax.swing.JLabel titulo_Lb;
     // End of variables declaration//GEN-END:variables
 }
